155de326ea0b0c93f8a258c1554f62c4a95e2efa
codershri <shrirang.a.sonawane43@gmail.com>
2026-08-16 01:36:35 +0530

    This is just revision code after a year
    Just tried piece of code

---
+++ Day1/Day1.py
@@ -0,0 +1,20 @@
+fruits = ["apple","banana","cherry"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
+
+def myfunction(num1,num2):
+    if num1>num2:
+        print("small")
+    else:
+        print("big")
+
+myfunction(10,15)
+
+#collection
+'''
+List, tuple if i have values stored in collections then python allows to unpack or retrive those values 
+into variables.
+'''
+
