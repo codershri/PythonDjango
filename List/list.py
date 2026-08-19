53e2750df0bf192502d4ccc0c28ec264559758cd
codershri <shrirang.a.sonawane43@gmail.com>
2026-08-19 23:35:52 +0530

    Update list.py

--- List/list.py
+++ List/list.py
@@ -1,5 +1,18 @@
 mylist=["apple","banana","orange","grape"]
-
+mylist2=[1,2,3,4,5,6,7,8,9,0]
+mylist3=[True,False,True]
 def Listfun():
     print(mylist)
 Listfun()
+
+def FindLength():
+    print(len(mylist))
+FindLength()
+
+def DataTypeOfList():
+    print(type(mylist))
+    print(type(mylist2))
+    print(type(mylist3))
+    global list4 
+    list4 = list(("x","y","z"))
+DataTypeOfList()
