9e336e03472bece8e78c4e4c09daa9999e2071c2
codershri <shrirang.a.sonawane43@gmail.com>
2026-08-24 00:43:12 +0530

    Update list2.py

--- List/list2.py
+++ List/list2.py
@@ -1,13 +1,23 @@
 fruits=['apple', 'chicken Alfredo', 'orange', 'grape', 'kiwi', 'mango', 'lichi', 'jamun']
 
-newlist = []
 
-for x in fruits:
-  if "a" in x:
-    newlist.append(x)
 
-print(newlist)
+def forloopwithLettermatch():
+    newlist = []
+    for x in fruits:
+        if "a" in x:
+          newlist.append(x)
 
-newlist = [x for x in fruits if "a" in x]
+    print(newlist)
 
-print(newlist)
+    newlist = [x for x in fruits if "a" in x]
+
+    print(newlist)
+forloopwithLettermatch()
+
+def sortingListAlphnumerically():
+    fruits.sort()
+    print("Acending order-->",fruits)
+    fruits.sort(reverse=True)
+    print("Decending order-->",fruits)
+sortingListAlphnumerically()
