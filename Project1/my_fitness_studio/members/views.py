521e0d1a48d00965c11279bf2f8d276e9a50bce6
codershri <shrirang.a.sonawane43@gmail.com>
2026-08-26 23:45:46 +0530

    Update database and update templates

--- Project1/my_fitness_studio/members/views.py
+++ Project1/my_fitness_studio/members/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 # Create your views here.
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('All_member_model_data.html')
+    context = {
+        'mymembers' : mymembers,
+    }
+
+
+    #template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render(context,request))
