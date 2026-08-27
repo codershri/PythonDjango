36295125d38c2451b0818374b2fad23e8d07a8f9
codershri <shrirang.a.sonawane43@gmail.com>
2026-08-28 00:36:20 +0530

    Update tuple.py

--- Tuple/tuple.py
+++ Tuple/tuple.py
@@ -1,9 +1,12 @@
 thistuple = ("apple","banana","grape","apple","coconut")
 emptytuple = ()
 singlevaluetuple = ("tiger",)
+tuple1 = ("abc", 34, True, 40, "male")
+
 print(type(thistuple))
 print(type(emptytuple))
 print(type(singlevaluetuple))
 
 print(thistuple)
 print(singlevaluetuple)
+print(tuple1)
